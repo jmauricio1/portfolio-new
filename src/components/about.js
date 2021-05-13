10ccefe365875a9bd78e2a6ebe4dbbd762a4103b
Joshua Jacob Mauricio <jmauricio15@wou.edu>
2021-05-12 21:12:02 -0700

    Updated the about section with the image of myself

--- src/components/about.js
+++ src/components/about.js
@@ -7,7 +7,7 @@ function About() {
       <h2 id="about-title">
         I develop web apps <br /> & software
       </h2>
-      <img id="about-img" src="/images/temp-about.jpg" alt="temp" />
+      <img id="about-img" src="/images/about.jpg" alt="temp" />
       <Container>
         <h3>About</h3>
         <h4>
@@ -17,22 +17,24 @@ function About() {
           Hunt
         </h4>
         <p>
+          Hello, I'm Josh! I'm an aspiring developer with years of experience in
+          developing software and web applications. I received my Bachelor of
+          Science degree in Computer Science from Western Oregon University in
+          2020. Post-graduation, I was also able to work towards and earn my
+          certificate of completion from The Complete 2021 Web Development
+          Bootcamp instructed by Dr. Angela Yu with the App Brewery. Since then,
+          I have been involved in coding communities such as FreeCodeCamp and
+          the App Brewery providing help to those learning software and web
+          development.
+          <br />
+          <br />
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Fermentum
-          leo vel orci porta non pulvinar neque laoreet suspendisse. Tellus
-          pellentesque eu tincidunt tortor aliquam nulla. Nullam non nisi est
-          sit amet facilisis magna etiam tempor.
-          <br />
-          <br />
-          Sapien pellentesque habitant morbi tristique senectus et netus. Sapien
-          et ligula ullamcorper malesuada. Consequat id porta nibh venenatis
-          cras sed felis eget velit. Arcu odio ut sem nulla pharetra diam sit
-          amet.
-          <br />
-          <br />
-          Ultrices in iaculis nunc sed augue. Aliquet porttitor lacus luctus
-          accumsan. Ut placerat orci nulla pellentesque. At quis risus sed
-          vulputate odio ut enim blandit.
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
         </p>
       </Container>
     </section>
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 function Navigation() {
   const [navClicked, setNavClicked] = useState(false);
 
-  const linkList = ["", "about", "projects", "contact"];
+  const linkList = ["", "about", "skills", "projects", "contact"];
 
   function handleNavClick() {
     if (navClicked) {
@@ -37,7 +37,7 @@ function Navigation() {
       <ul
         id="nav-menu"
         style={{
-          width: navClicked ? "600px" : "0",
+          width: navClicked ? "500px" : "0",
           opacity: navClicked ? "1" : "0",
           transition: "1s ease-in-out",
           transitionDelay: navClicked ? "0s" : "0.75s",
